e90295fb25d252faf56982089ab08a04a18b2086
Joao Gabriel Henz <joao.henz.pb@compasso.com.br>
2022-06-04 16:56:13 -0300

    Feat: goBack button but not defined yet

--- script/script.js
+++ script/script.js
@@ -1,6 +1,10 @@
 //TESTING JAVASCRIPT
 
+document.getElementById("s-panel").style.display = "none";
+
 function register() {
+
+    let bIsOK = true;
 
     //Main form Validatios
     let errorArray = ["Name", "Email", "Password", "Phone", "Birthday"]
@@ -14,6 +18,7 @@ function register() {
     //Name validation check
     if(inputForm[0].value.length < 4 || !inputForm[0].value.match(/[ ]/g)) {
         errorMsg[0].innerHTML = errorArray[0] + " Invalid";
+        bIsOK = false;
     } else {
         errorMsg[0].innerHTML = "";
     }
@@ -21,6 +26,7 @@ function register() {
     //Email validation check
     if(inputForm[1].value.match(/[A-Z]/) || inputForm[1].value.match(/[À-ú]/) || inputForm[1].value.length == 0) {
         errorMsg[1].innerHTML = errorArray[1] + " Invalid";
+        bIsOK = false;
     } else {
         errorMsg[1].innerHTML = "";
     }
@@ -28,6 +34,7 @@ function register() {
     //Password validation check
     if(inputForm[2].value.match(/[^6-9]/) || inputForm[1].value.length == 0) {
         errorMsg[2].innerHTML = errorArray[2] + " Invalid";
+        bIsOK = false;
     } else {
         errorMsg[2].innerHTML = "";
     }
@@ -35,6 +42,7 @@ function register() {
     //Phone validation check
     if(inputForm[3].value.length != 11 || inputForm[3].value.match(/\D/g)) {
         errorMsg[3].innerHTML = errorArray[3] + " Invalid";
+        bIsOK = false;
     } else {
         errorMsg[3].innerHTML = "";
     }
@@ -42,8 +50,9 @@ function register() {
     //Date validation check
     let date = new Date(inputForm[4].value);
     let age = 2022 - date.getFullYear() 
-    if(age < 18) {
+    if(age < 18 || date == "Invalid Date") {
         errorMsg[4].innerHTML = errorArray[4] + " Invalid";
+        bIsOK = false;
     } else {
         errorMsg[4].innerHTML = "";
     }
@@ -53,8 +62,15 @@ function register() {
 
     //const checkBox = document.getEl
 
+    //
+    if(bIsOK) {  
+        document.getElementById("s-panel").style.display = "flex";
+        document.getElementById("m-panel").style.display = "none";
+    }
+
 }
 
 function goBack() {
-    
+    document.getElementById("s-panel").style.display = "none";
+    document.getElementById("m-panel").style.display = "flex";
 }
